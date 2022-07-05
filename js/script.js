a39c62d9ac734d8be94a7c13c506a370148e4527
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-05 14:51:40 +0200

    add function btn

--- js/script.js
+++ js/script.js
@@ -1,36 +1,54 @@
 // collego js a vue con la variabile 
 
 const app = new Vue({
-    el : '#root' , 
-    //? dichiaro l'index come parametro di riferimento
-    index : 0
-    //* creo il contenitore slides contenente tutte le indicazione delle img
-    slides :  [
-                    {
-                        image: 'img/01.jpg',
-                        title: 'Svezia',
-                        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                    },
-                    {
-                        image: 'img/02.jpg',
-                        title: 'Svizzera',
-                        text: 'Lorem ipsum.',
-                    },
-                    {
-                        image: 'img/03.jpg',
-                        title: 'Gran Bretagna',
-                        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                    },
-                    {
-                        image: 'img/04.jpg',
-                        title: 'Germania',
-                        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-                    },
-                    {
-                        image: 'img/05.jpg',
-                        title: 'Paradise',
-                        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-                    }
-             ];
-            
+    //! colego tramite id
+    el : '#app' , 
+    //! creo i dati che poi inserirò
+    data : {
+        //? dichiaro l'index come parametro di riferimento
+        index : 0,
+        //* creo il contenitore slides contenente tutte le indicazione delle img
+        slides :  [
+                        {
+                            image: 'img/01.jpg',
+                            title: 'Svezia',
+                            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                        },
+                        {
+                            image: 'img/02.jpg',
+                            title: 'Svizzera',
+                            text: 'Lorem ipsum.',
+                        },
+                        {
+                            image: 'img/03.jpg',
+                            title: 'Gran Bretagna',
+                            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                        },
+                        {
+                            image: 'img/04.jpg',
+                            title: 'Germania',
+                            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                        },
+                        {
+                            image: 'img/05.jpg',
+                            title: 'Paradise',
+                            text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                        }
+                ]
+    },
+    //! creo i methods
+    methods : {
+         btnScrolling : function(isNext){
+
+            //* creo un ternary operetor per vedere se il btn e next o previus
+            this.index = (isNext) ? (this.index + 1) : (this.index - 1);
+            //* creo un if per il ciclo infinito
+            if(this.index === this.slides.length){
+                this.index = 0 ;
+            } else if (this.index === -1){
+                this.index = this.slides.length -1;
+            }
+            return this.index;
+         },
+    }
 })
